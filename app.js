e9bdd65ebc0ec7047d353992d8cab819438b1973
Arjun Das <arjundasback515@gmail.com>
2022-09-01 23:58:15 +0600

    fetch the data and serch option addedd

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("appppppppppppppp");
+const searchBook= () =>{
+    const serchValue = document.getElementById("search-value");
+    const value = serchValue.value;
+    fetch(`http://openlibrary.org/search.json?q=${value}`)
+    .then(res=> res.json())
+    .then(data=>console.log(data.docs))
+} 
+
