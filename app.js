8e0b05b26ca527e35a121e86177b5c30dad26312
Arjun Das <arjundasback515@gmail.com>
2022-09-02 12:42:21 +0600

    book image added

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const searchBook= () =>{
+const searchBook= (v) =>{
     const serchValue = document.getElementById("search-value");
     const value = serchValue.value;
     fetch(`http://openlibrary.org/search.json?q=${value}`)
     .then(res=> res.json())
     .then(data=>displayBook(data.docs))
+
+    serchValue.value="";
 } 
 
 
 const displayBook= (book) =>{
 console.log(book);
 const serachResult = document.getElementById("search-result");
+serachResult.innerHTML=""
 book.forEach(books => {
-    const {title,author_name,publisher,publish_date}=books
+    const {title,author_name,publisher,publish_date,cover_i}=books
     const create = document.createElement("div")
     create.innerHTML=`
     <div class="col">
                     <div class="card h-100">
+                        <img src="https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Book-title: ${title}</h5>
                             <h5>Author_Name: ${author_name}</h5>
@@ -38,3 +42,5 @@ document.getElementById("search-value").addEventListener("keypress",function(e){
         searchBook();
       }
 })
+
+searchBook("helloworld")
