2c28c5bec2b9a94bad97ad8b288f43e7fc0dff87
Arjun Das <arjundasback515@gmail.com>
2022-09-02 12:25:14 +0600

    card dynamic

--- app.js
+++ app.js
@@ -11,17 +11,19 @@ const displayBook= (book) =>{
 console.log(book);
 const serachResult = document.getElementById("search-result");
 book.forEach(books => {
+    const {title,author_name,publisher,publish_date}=books
     const create = document.createElement("div")
     create.innerHTML=`
     <div class="col">
                     <div class="card h-100">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">Book-title: ${title}</h5>
+                            <h5>Author_Name: ${author_name}</h5>
+                            <h5>Publisher: ${publisher}</h5>
+                            
                         </div>
                         <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
+                            <small class="text-muted">Last updated ${publish_date}</small>
                         </div>
                     </div>
                 </div>
